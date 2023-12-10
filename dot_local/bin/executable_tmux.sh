45f9da80224e7c561b6c7461dda6be5920939bea
nika87sha <vero.dg1987@gmail.com>
2023-12-10 19:52:21 +0100

    add modify script tmux sessions

--- dot_local/bin/executable_tmux.sh
+++ dot_local/bin/executable_tmux.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # Session 1 Work
-tmux new-session -s Work -n zsh -d
-tmux new-window -t Work -d -n MP
-tmux new-window -t Work -d -n vim
-tmux new-window -t Work -d -n btop
-tmux new-window -t Work -d -n fm
+tmux new-session -s Work -d
+tmux new-window -t Work -d
+tmux new-window -t Work -d
+tmux new-window -t Work -d
+tmux new-window -t Work -d
 
-
-tmux select-window -t Work:zsh
+tmux select-window -t Work 'zsh'
 tmux split-window -v -p 50
-tmux send-keys -t Work:zsh "zi" Enter
-
-tmux send-keys -t Work:MP "ncmpcpp" Enter
-tmux send-keys -t Work:btop "btop" Enter
-tmux send-keys -t Work:vim "/usr/bin/nvim" Enter
-tmux send-keys -t Work:fm "vifm" Enter
+tmux send-keys -t Work:1 'zi' Enter
+tmux send-keys -t Work:2 'ncmpcpp' Enter
+tmux send-keys -t Work:3 'btop' Enter
+tmux send-keys -t Work:4 '/usr/bin/nvim' Enter
+tmux send-keys -t Work:5 'vifm' Enter
 
 # Session 2 DevOps
-tmux new-session -s DevOps -n Docker -d
+tmux new-session -s DevOps -d
+tmux new-window -t DevOps -d
 
-tmux select-window -t DevOps:Docker
+tmux select-window -t DevOps -d 'lazydocker'
 tmux split-window -v -p 50
-tmux send-keys -t DevOps:Docker "navi" Enter
-tmux send-keys -t DevOps:Docker "lazydocker" Enter
+tmux send-keys -t DevOps:1 'oxker' Enter
+tmux send-keys -t DevOps:2 'nvim +"DBUI"' Enter
+
 
 # Attach Session, on the Main window
 tmux -u attach -t Work
