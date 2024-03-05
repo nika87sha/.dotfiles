1368b9678a1e165e5953cee8a61d48f625d13ad4
Verónica Durán <veronica.duran87@protonmail.com>
2024-03-05 11:10:15 +0100

    Delete dot_config directory

--- dot_config/nvim2k/init.lua
+++
@@ -1,8 +0,0 @@
-require('core.options')
-require('core.functions')
-require('core.keymaps')
-require('core.autocmd')
-require('plugins.lazy')
-require('plugins.lang.neodev')
-require('plugins.lang.lsp-zero')
-require'lspconfig'.pyright.setup{}
